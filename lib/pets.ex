2d2856879c0a089145e72465153cbda37afc9c90
andyl <andy@r210.com>
2019-08-09 07:50:31 -0700

    Refactor signature

--- lib/pets.ex
+++ lib/pets.ex
@@ -1,29 +1,61 @@
 defmodule Pets do
-  @moduledoc "A generic data-store using PersistentEts."
+  @moduledoc """
+  A generic datastore using PersistentEts.
 
-  @doc "Start the data-store with tablekey and filepath."
-  def start(tablekey, filepath, opts \\ []) do
-    unless started?(tablekey) do
+  Most Pets functions take a `signature` as the first argument, which
+  identifies a specific Pets datastore.
+  
+  The signature is simply a map with two fields:
+
+  - tablekey - an atom which identifies the underlying ETS table
+  - filepath - the path in which to store the PersistentEts datafile
+
+  You can create a new Pets signature in many ways:
+
+  ```
+  $> x = %Pets{}
+  $> y = Pets.test_sig()
+  $> z = %{tablekey: :asdf, filepath: "/tmp/myfile.data"}
+  ```
+
+  Pets is generally wrapped in a container module for managing specific types
+  of records.  The container module is responsible for establishing the
+  signature, and defining a struct that is stored in PersistentEts.
+  """
+
+  defstruct [:tablekey, :filepath]
+
+  @doc "Start the datastore."
+  def start(sig, opts \\ []) do
+    unless started?(sig) do
       tableopts = Enum.uniq([:named_table, :public] ++ opts)
-      PersistentEts.new(tablekey, filepath, tableopts)
+      PersistentEts.new(sig.tablekey, sig.filepath, tableopts)
     end
   end
 
   @doc "Stop the database if it is running, then start."
-  def restart(tablekey, filepath, opts \\ []) do
-    if started?(tablekey), do: stop(tablekey)
-    start(tablekey, filepath, opts)
+  def restart(sig, opts \\ []) do
+    if started?(sig), do: stop(sig)
+    start(sig, opts)
   end
 
-  @doc "Stop the data-store and remove the data-file."
-  def stop(tablekey, filepath) do
-    stop(tablekey)
-    File.rm(filepath)
+  @doc "Stop the datastore."
+  def stop(sig) do
+    if started?(sig) do
+      try do
+        PersistentEts.delete(sig.tablekey)
+      rescue
+        _ -> :error
+      end
+    end
+    :ok
   end
 
-  @doc "Stop the data-store."
-  def stop(tablekey) do
-    if started?(tablekey), do: PersistentEts.delete(tablekey)
+  @doc "Stop the datastore and remove the data-file."
+  def cleanup(sig) do
+    stop(sig)
+    File.rm(sig.filepath)
+    :ok
   end
 
   @doc """
@@ -31,8 +63,9 @@ defmodule Pets do
 
   The datakey is the first element in the tuple.
   """
-  def insert(tablekey, tuple) do
-    :ets.insert(tablekey, tuple)
+  def insert(sig, tuple) do
+    start(sig)
+    :ets.insert(sig.tablekey, tuple)
   end
 
   @doc """
@@ -40,8 +73,9 @@ defmodule Pets do
 
   The datakey is the first element in the tuple.
   """
-  def lookup(tablekey, datakey) do
-    result = :ets.lookup(tablekey, datakey)
+  def lookup(sig, datakey) do
+    start(sig)
+    result = :ets.lookup(sig.tablekey, datakey)
 
     case result do
       [] -> nil
@@ -50,26 +84,30 @@ defmodule Pets do
   end
 
   @doc "Return all records in the table."
-  def all(tablekey) do
-    :ets.tab2list(tablekey)
+  def all(sig) do
+    start(sig)
+    :ets.tab2list(sig.tablekey)
   end
  
   @doc "Check for existence of key in data-store."
-  def has_key?(tablekey, datakey) do
-    :ets.lookup(tablekey, datakey) != []
+  def has_key?(sig, datakey) do
+    start(sig)
+    :ets.lookup(sig.tablekey, datakey) != []
   end
 
   @doc "Return true if a table has been started."
-  def started?(tablekey) do
-    :ets.whereis(tablekey) != :undefined
+  def started?(sig) do
+    :ets.whereis(sig.tablekey) != :undefined
   end
 
   @doc "Generate a test context."
-  def test_context do
+  def test_sig do test_sig(prefix: "pets") end
+
+  def test_sig([prefix: pref]) do
     with num <- Enum.random(10000..99999),
          do: %{
-           tablekey: String.to_atom("link_#{num}"),
-           filepath: "/tmp/store_test_#{num}.dat"
+           tablekey: String.to_atom("#{pref}_test_#{num}"),
+           filepath: "/tmp/#{pref}_test_#{num}.dat"
          }
   end
 end
--- test/pets_test.exs
+++ test/pets_test.exs
@@ -2,91 +2,95 @@ defmodule PetsTest do
   use ExUnit.Case, async: true
 
   setup do
-    Pets.test_context()
+    sig = Pets.test_sig()
+    on_exit(fn -> Pets.cleanup(sig) end)
+    sig
   end
 
-  describe "#start / #stop" do
-    test "creates a table", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.started?(ctx.tablekey)
-      assert File.exists?(ctx.filepath)
-      cleanup(ctx)
+  describe "#start / #stop / #cleanup" do
+    test "creates a table", sig do
+      assert Pets.start(sig)
+      assert Pets.started?(sig)
+      assert File.exists?(sig.filepath)
     end
 
-    test "restarts with an existing filepath", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.started?(ctx.tablekey)
-      Pets.stop(ctx.tablekey)
-      refute Pets.started?(ctx.tablekey)
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.started?(ctx.tablekey)
-      cleanup(ctx)
+    test "restarts with an existing filepath", sig do
+      assert Pets.start(sig)
+      Pets.stop(sig)
+      refute Pets.started?(sig)
+      assert File.exists?(sig.filepath)
+    end
+
+    test "remove file on cleanup", sig do
+      assert Pets.start(sig)
+      assert Pets.cleanup(sig)
+      refute Pets.started?(sig)
+      refute File.exists?(sig.filepath)
+    end
+
+    test "stop w/o starting", sig do
+      assert Pets.stop(sig)
+    end
+
+    test "cleanup w/o starting", sig do
+      assert Pets.cleanup(sig)
     end
   end
 
   describe "#insert / #lookup" do
-    test "sets and gets data", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.insert(ctx.tablekey, {:asdf, 1})
-      assert Pets.lookup(ctx.tablekey, :asdf) == [asdf: 1]
-      cleanup(ctx)
+    test "sets and gets data", sig do
+      assert Pets.start(sig)
+      assert Pets.insert(sig, {:asdf, 1})
+      assert Pets.lookup(sig, :asdf) == [asdf: 1]
     end
 
-    test "preserves data between restarts", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.insert(ctx.tablekey, {:qwer, 1})
-      assert Pets.lookup(ctx.tablekey, :qwer) == [qwer: 1]
-      Pets.stop(ctx.tablekey)
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.lookup(ctx.tablekey, :qwer) == [qwer: 1]
-      cleanup(ctx)
+    test "without explicit start", sig do
+      assert Pets.insert(sig, {:asdf, 1})
+      assert Pets.lookup(sig, :asdf) == [asdf: 1]
     end
 
-    test "updates values", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.insert(ctx.tablekey, {"asdf", 1})
-      assert Pets.lookup(ctx.tablekey, "asdf") == [{"asdf", 1}]
-      assert Pets.insert(ctx.tablekey, {"asdf", 2})
-      assert Pets.lookup(ctx.tablekey, "asdf") == [{"asdf", 2}]
-      cleanup(ctx)
+    test "preserves data between restarts", sig do
+      assert Pets.insert(sig, {:qwer, 1})
+      assert Pets.lookup(sig, :qwer) == [qwer: 1]
+      Pets.stop(sig)
+      assert Pets.start(sig)
+      assert Pets.lookup(sig, :qwer) == [qwer: 1]
+    end
+
+    test "updates values", sig do
+      assert Pets.insert(sig, {"asdf", 1})
+      assert Pets.lookup(sig, "asdf") == [{"asdf", 1}]
+      assert Pets.insert(sig, {"asdf", 2})
+      assert Pets.lookup(sig, "asdf") == [{"asdf", 2}]
     end
   end
 
   describe "#has_key?" do
-    test "returns false if key does not exist", ctx do
-      Pets.start(ctx.tablekey, ctx.filepath)
-      refute Pets.has_key?(ctx.tablekey, "randomkey")
+    test "returns false if key does not exist", sig do
+      Pets.start(sig)
+      refute Pets.has_key?(sig, "randomkey")
     end
 
-    test "returns true if key exists", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.insert(ctx.tablekey, {"asdf", 1})
-      assert Pets.has_key?(ctx.tablekey, "asdf")
+    test "without explicit start", sig do
+      refute Pets.has_key?(sig, "randomkey")
     end
-  end
 
-  describe "#stop" do
-    test "cleans up the ETS tablekey and filepath", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath)
-      assert Pets.started?(ctx.tablekey)
-      assert File.exists?(ctx.filepath)
-      cleanup(ctx)
-      refute Pets.started?(ctx.tablekey)
-      refute File.exists?(ctx.filepath)
+    test "returns true if key exists", sig do
+      assert Pets.insert(sig, {"asdf", 1})
+      assert Pets.has_key?(sig, "asdf")
+      refute Pets.has_key?(sig, "qwer")
     end
   end
 
   describe "bag datastores" do
-    test "stores all inserts", ctx do
-      assert Pets.start(ctx.tablekey, ctx.filepath, [:bag])
-      assert Pets.insert(ctx.tablekey, {"asdf", 1})
-      assert Pets.lookup(ctx.tablekey, "asdf") == [{"asdf", 1}]
-      assert Pets.insert(ctx.tablekey, {"asdf", 2})
-      assert Pets.lookup(ctx.tablekey, "asdf") == [{"asdf", 1}, {"asdf", 2}]
-      cleanup(ctx)
+    test "stores all inserts", sig do
+      assert Pets.start(sig, [:bag])
+      assert Pets.insert(sig, {"asdf", 1})
+      assert Pets.lookup(sig, "asdf") == [{"asdf", 1}]
+      assert Pets.insert(sig, {"asdf", 2})
+      assert Pets.lookup(sig, "asdf") == [{"asdf", 1}, {"asdf", 2}]
+      assert Pets.insert(sig, {"asdf", 1})
+      assert Pets.lookup(sig, "asdf") == [{"asdf", 1}, {"asdf", 2}]
     end
   end
-
-  defp cleanup(ctx), do: Pets.stop(ctx.tablekey, ctx.filepath)
-
 end
